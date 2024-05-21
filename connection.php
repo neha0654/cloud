dc7b1f988c2388f50a9f5039a6ec88e54ff9a0bb
neha0654 <143345507+neha0654@users.noreply.github.com>
2024-05-21 12:16:13 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $database= new mysqli("php-database.cqbtncjorbsc.us-east-1.rds.amazonaws.com","admin","12345678","edoc");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
-?>
